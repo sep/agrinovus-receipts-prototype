69ff384a05aff6407950aad0bb7a9916a18e0602
Giuliana E. Taylor <getaylor@sep.com>
2020-11-16 12:22:08 -0500

    Capture image from camera

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,118 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-cypress" target="_blank" rel="noopener">e2e-cypress</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class='container'>
+    <div class='row'>
+      <div class='col-md-6'>
+        <h2>Current Camera</h2>
+        <code v-if='device'>{{ device.label }}</code>
+        <div class='border'>
+          <web-cam
+            ref='webcam'
+            :device-id='deviceId'
+            width='100%'
+            @started='onStarted'
+            @stopped='onStopped'
+            @error='onError'
+            @cameras='onCameras'
+            @camera-change='onCameraChange'
+          />
+        </div>
+
+        <div class='row'>
+          <div class='col-md-12'>
+            <select v-model='camera'>
+              <option>-- Select Device --</option>
+              <option
+                v-for='device in devices'
+                :key='device.deviceId'
+                :value='device.deviceId'
+              >
+                {{ device.label }}
+              </option>
+            </select>
+          </div>
+          <div class='col-md-12'>
+            <button type='button' class='btn btn-primary' @click='onCapture'>
+              Capture Photo
+            </button>
+            <button type='button' class='btn btn-danger' @click='onStop'>
+              Stop Camera
+            </button>
+            <button type='button' class='btn btn-success' @click='onStart'>
+              Start Camera
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class='col-md-6'>
+        <h2>Captured Image</h2>
+        <figure class='figure'>
+          <img :src='img' class='img-responsive' />
+        </figure>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { WebCam } from 'vue-web-cam';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String,
+  components: { WebCam },
+  data() {
+    return {
+      img: null,
+      camera: null,
+      deviceId: null,
+      devices: [],
+    };
+  },
+  computed: {
+    device() {
+      return this.devices.find((n) => n.deviceId === this.deviceId);
+    },
+  },
+  watch: {
+    camera(id) {
+      this.deviceId = id;
+    },
+    devices() {
+      // Once we have a list select the first one
+      const { first } = this.devices;
+      if (first) {
+        this.camera = first.deviceId;
+        this.deviceId = first.deviceId;
+      }
+    },
+  },
+  methods: {
+    onCapture() {
+      this.img = this.$refs.webcam.capture();
+    },
+    onStarted(stream) {
+      console.log('On Started Event', stream);
+    },
+    onStopped(stream) {
+      console.log('On Stopped Event', stream);
+    },
+    onStop() {
+      this.$refs.webcam.stop();
+    },
+    onStart() {
+      this.$refs.webcam.start();
+    },
+    onError(error) {
+      console.log('On Error Event', error);
+    },
+    onCameras(cameras) {
+      this.devices = cameras;
+      console.log('On Cameras Event', cameras);
+    },
+    onCameraChange(deviceId) {
+      this.deviceId = deviceId;
+      this.camera = deviceId;
+      console.log('On Camera Change Event', deviceId);
+    },
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
