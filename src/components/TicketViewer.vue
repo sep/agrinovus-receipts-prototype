eaf8d3579659bec10a6be009f18d67fb25ea60cf
Giuliana E. Taylor <getaylor@sep.com>
2020-11-23 09:15:08 -0500

    Display tickets with "Paid" and "Processed" badges

--- src/components/TicketViewer.vue
+++ src/components/TicketViewer.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class='container p-4'>
+  <div class='p-4'>
     <h1>View Tickets</h1>
+    <b-card-group deck :key="img.pathShort" v-for="(img, i) in images">
+        <b-card overlay :img-src="img.pathLong" align="right">
+            <b-card-text>
+                <b-badge class='mr-1' variant="primary" v-if="i % 2 == 0">Processed</b-badge>
+                <b-badge variant="success">Paid</b-badge>
+            </b-card-text>
+        </b-card>
+    </b-card-group>
   </div>
 </template>
 
 <script>
 export default {
   name: 'TicketViewer',
+  data() {
+    return {
+      images: [],
+    };
+  },
+  mounted() {
+    this.importAll(require.context('@/assets/tickets/', true, /\.png$/));
+  },
+  methods: {
+    importAll(r) {
+      r.keys().forEach(key => (this.images.push({ pathLong: r(key), pathShort: key })));
+    },
+  },
 };
 </script>
