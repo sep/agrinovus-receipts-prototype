0a1695bafc90b71b5c1069629f1023489e1f1955
Giuliana E. Taylor <getaylor@sep.com>
2020-11-24 10:45:07 -0500

    Correct example spec file

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,10 +3,8 @@ import TicketCaptor from '@/components/TicketCaptor.vue';
 
 describe('TicketCaptor.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(TicketCaptor, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+    const wrapper = shallowMount(TicketCaptor);
+
+    expect(wrapper.text()).toContain('Capture Ticket');
   });
 });
