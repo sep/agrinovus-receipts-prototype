a210acf7bb1741b2ae6f2ddd7b8678645765b382
Giuliana E. Taylor <getaylor@sep.com>
2020-11-30 13:31:16 -0500

    Group Tickets by Elevator

---
+++ src/components/TicketListByElevator.vue
@@ -0,0 +1,37 @@
+<template>
+  <b-container>
+    <h2 class='text-muted'>Tickets by Elevator</h2>
+    <div class="accordion" role="tablist">
+      <template v-for="(group, name, i) in groupMap">
+        <b-card :key="i" no-body class="mb-1">
+          <b-card-header header-tag="header" class="p-1" role="tab">
+          <b-button block v-b-toggle="`accordion-${i}`" variant="info">{{name}}</b-button>
+          </b-card-header>
+          <b-collapse
+          :id="`accordion-${i}`"
+          accordion="my-accordion"
+          role="tabpanel"
+          >
+            <b-card-body>
+              <b-card-group deck :key="i" v-for="(img, i) in group">
+                <b-card overlay :img-src="img.pathLong" align="right">
+                  <b-card-text>
+                    <b-badge class='mr-1' variant="primary" v-if="i % 2 == 0">Processed</b-badge>
+                    <b-badge variant="success">Paid</b-badge>
+                  </b-card-text>
+                </b-card>
+              </b-card-group>
+            </b-card-body>
+          </b-collapse>
+        </b-card>
+      </template>
+    </div>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'TicketListByElevator',
+  props: ['groupMap'],
+};
+</script>
--- src/components/TicketViewer.vue
+++ src/components/TicketViewer.vue
@@ -1,38 +1,57 @@
 <template>
   <b-container>
     <h1>View Tickets</h1>
-    <b-card-group deck :key="img.pathShort" v-for="(img, i) in images">
-        <b-card overlay :img-src="img.pathLong" align="right">
-            <b-card-text>
-                <b-badge class='mr-1' variant="primary" v-if="i % 2 == 0">Processed</b-badge>
-                <b-badge variant="success">Paid</b-badge>
-            </b-card-text>
-        </b-card>
-    </b-card-group>
+    <ticket-list-by-elevator :group-map="this.groupsByElevator"></ticket-list-by-elevator>
   </b-container>
 </template>
 
 <script>
+import TicketListByElevator from '@/components/TicketListByElevator.vue';
+
 export default {
   name: 'TicketViewer',
+  components: {
+    'ticket-list-by-elevator': TicketListByElevator,
+  },
   data() {
     return {
       images: [],
     };
   },
+  computed: {
+    groupsByElevator() {
+      return this.images.reduce((map, obj) => {
+        const group = map[obj.name] || [];
+        group.push(obj);
+        // eslint-disable-next-line no-param-reassign
+        map[obj.name] = group;
+        return map;
+      }, {});
+    },
+  },
   mounted() {
     const staticImages = require.context('@/assets/tickets/', true, /\.png$/);
     this.importContext(staticImages);
 
-    const uploadedImages = JSON.parse(localStorage.getItem('uploadedImages'));
+    const uploadedImages = JSON.parse(localStorage.getItem('uploadedImages')) || [];
     this.importArray(uploadedImages);
   },
   methods: {
     importContext(r) {
-      r.keys().forEach((key) => (this.images.push({ pathLong: r(key), pathShort: key })));
+      const FILE_NAME_REGEX = /(.+)\/(.+)\.png$/;
+
+      r.keys().forEach((key) => (this.images.push({
+        pathLong: r(key),
+        pathShort: key,
+        name: key.replace(FILE_NAME_REGEX, '$2'),
+      })));
     },
     importArray(images) {
-      images.forEach((path, index) => this.images.push({ pathLong: path, pathShort: index }));
+      images.forEach((path, index) => this.images.push({
+        pathLong: path,
+        pathShort: index,
+        name: 'Home',
+      }));
     },
   },
 };
