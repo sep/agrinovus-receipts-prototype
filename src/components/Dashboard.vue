92d7b11d3259bb8a0f6decc28787b9c01b2b8334
Giuliana E. Taylor <getaylor@sep.com>
2020-12-03 12:10:40 -0500

    Embed new farmer dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -10,7 +10,7 @@
       <iframe src="https://app.powerbi.com/reportEmbed?reportId=0933ae98-db2e-46f3-a1d3-8afec39ddb75&autoAuth=true&ctid=898f7683-8ab4-4a28-b74e-510b95f981eb&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXVzLW5vcnRoLWNlbnRyYWwtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" frameborder="0" allowFullScreen="true"></iframe>
     </template>
     <template v-else>
-      <iframe src="https://app.powerbi.com/reportEmbed?reportId=38aa0560-fd21-44fd-a76e-0dfa8ba52371&autoAuth=true&ctid=898f7683-8ab4-4a28-b74e-510b95f981eb&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXVzLW5vcnRoLWNlbnRyYWwtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" frameborder="0" allowFullScreen="true"></iframe>
+      <iframe src="https://app.powerbi.com/reportEmbed?reportId=a0a65da5-66f7-4fdf-bd5d-b0bdb79ce672&appId=0e73e868-885f-443f-9871-a5bf13d43213&autoAuth=true&ctid=898f7683-8ab4-4a28-b74e-510b95f981eb&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXVzLW5vcnRoLWNlbnRyYWwtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" frameborder="0" allowFullScreen="true"></iframe>
     </template>
   </div>
 </template>
