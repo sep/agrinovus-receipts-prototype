5c46ada012911c4add3ec5924d3c87569e43505a
Giuliana E. Taylor <getaylor@sep.com>
2020-12-02 21:55:59 -0500

    Group tickets by account

---
+++ src/components/TicketListByAccount.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-container>
+    <h3 class='text-muted'>Tickets by Account</h3>
+    <div class="accordion" role="tablist">
+      <template v-for="(group, name, i) in groupMap">
+        <b-card :key="i" no-body class="mb-1">
+          <b-card-header header-tag="header" class="p-1 text-left">
+            {{name}}
+          </b-card-header>
+          <b-card-body>
+            <b-card-group deck :key="i" v-for="(img, i) in group">
+              <b-card img-left :img-src="img.pathLong" align="right">
+                <b-card-text>
+                  <b-row>
+                    <b-col>{{ new Date().toDateString() }}</b-col>
+                    <b-col>967.33 BU</b-col>
+                    <b-col>
+                      <b-badge class='mr-1' variant="primary" v-if="i%2 == 0">Processed</b-badge>
+                      <b-badge variant="success">Paid</b-badge>
+                    </b-col>
+                  </b-row>
+                </b-card-text>
+              </b-card>
+            </b-card-group>
+          </b-card-body>
+        </b-card>
+      </template>
+    </div>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'TicketListByElevator',
+  props: ['groupMap'],
+};
+</script>
+
+<style scoped>
+img {
+  height: 100px;
+}
+</style>
--- src/components/TicketViewer.vue
+++ src/components/TicketViewer.vue
@@ -3,30 +3,63 @@
     <b-button to="/camera" v-b-tooltip.hover title="Add new ticket" class="mx-3 float-right">
       <b-icon-plus-circle/>
     </b-button>
-    <ticket-list-by-elevator :group-map="this.groupsByElevator"></ticket-list-by-elevator>
+    <b-form-group label="Group By">
+      <b-form-radio-group
+        v-model="selected"
+        :options="options"
+        buttons
+      ></b-form-radio-group>
+    </b-form-group>
+    <ticket-list-by-elevator v-if="selected == 0" :group-map="this.groupsByElevator">
+    </ticket-list-by-elevator>
+    <ticket-list-by-account v-if="selected == 1" :group-map="this.groupsByAccount">
+    </ticket-list-by-account>
   </div>
 </template>
 
 <script>
 import TicketListByElevator from '@/components/TicketListByElevator.vue';
+import TicketListByAccountVue from './TicketListByAccount.vue';
 
 export default {
   name: 'TicketViewer',
   components: {
     'ticket-list-by-elevator': TicketListByElevator,
+    'ticket-list-by-account': TicketListByAccountVue,
   },
   data() {
     return {
       images: [],
+      selected: 0,
+      options: [
+        { text: 'Elevator', value: 0 },
+        { text: 'Account', value: 1 },
+      ],
     };
   },
   computed: {
+    linkedAccounts() {
+      return this.$store.state.user.accounts;
+    },
     groupsByElevator() {
       return this.images.reduce((map, obj) => {
-        const group = map[obj.name] || [];
+        const groupName = obj.name;
+        const group = map[groupName] || [];
         group.push(obj);
         // eslint-disable-next-line no-param-reassign
-        map[obj.name] = group;
+        map[groupName] = group;
+        return map;
+      }, {});
+    },
+    groupsByAccount() {
+      return this.images.reduce((map, obj) => {
+        const randomIndex = Math.floor(Math.random() * this.linkedAccounts.length);
+        const randomAccount = this.linkedAccounts[randomIndex];
+        const groupName = randomAccount.name;
+        const group = map[groupName] || [];
+        group.push(obj);
+        // eslint-disable-next-line no-param-reassign
+        map[groupName] = group;
         return map;
       }, {});
     },
